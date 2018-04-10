8c54044e185d6c3da45737607e9a45f97607d4b4
Cristiano Coelho De Jesus Souza <871826@sga.pucminas.br>
2018-04-10 19:19:22 -0300

    Inicialização do projeto e estrutura switch

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("CONVERSOR DE UNIDADES\nSelecione uma opcao abaixo:\n");
+    printf("1 - Converter um numero decimal para binario.");
+    printf("2 - Converter um numero binario para decimal.");
+    printf("3 - Converter um numero decimal para octal.");
+    printf("4 - Converter um numero octal para decimal.");
+    switch (opcao) {
+    case 1:
+
+    break;
+
+    case 2:
+
+        break;
+
+    case 3:
+
+        break;
+
+    case 4:
+
+        break;
+    }
+    printf("Hello world!\n");
+    return 0;
+}
