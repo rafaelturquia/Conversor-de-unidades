44e21ec42018198ce7eb4341e42af2a7a0a09fbc
Cristiano Coelho De Jesus Souza <871826@sga.pucminas.br>
2018-04-10 20:33:32 -0300

    Inicio da opcao 3

--- main.c
+++ main.c
@@ -3,26 +3,34 @@
 
 int main()
 {
+    int opcao;
+    float numDecimal, numOctal;
     printf("CONVERSOR DE UNIDADES\nSelecione uma opcao abaixo:\n");
-    printf("1 - Converter um numero decimal para binario.");
-    printf("2 - Converter um numero binario para decimal.");
-    printf("3 - Converter um numero decimal para octal.");
-    printf("4 - Converter um numero octal para decimal.");
-    switch (opcao) {
+    printf("1 - Converter um numero decimal para binario.\n");
+    printf("2 - Converter um numero binario para decimal.\n");
+    printf("3 - Converter um numero decimal para octal.\n");
+    printf("4 - Converter um numero octal para decimal.\n");
+    scanf("%d", &opcao);
+        system("cls");
+    switch (opcao)
+    {
     case 1:
-
-    break;
+        printf("--Decimal para Binario--\n\n");
+        break;
 
     case 2:
-
+        printf("--Binario para Decimal--\n\n");
         break;
 
     case 3:
+        printf("--Decimal para Octal--\n\n");
+        printf("Digite o numero para ser convertido: ");
+        scanf("%f", &numDecimal);
 
         break;
 
     case 4:
-
+        printf("--Octal para Decimal--\n\n");
         break;
     }
     printf("Hello world!\n");
